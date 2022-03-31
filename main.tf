309800e23ea6ea469a80a0f6fe22174551b90d7c
Ryan Tiffany <ryantiffany@fastmail.com>
2022-03-31 11:30:54 -0500

    Test with count object

--- main.tf
+++ main.tf
@@ -1,13 +1,39 @@
+locals {}
+
 variable "region" {
   default = "us-south"
 }
 
-data "external" "env" { program = ["jq", "-n", "env"] }
-
-output "data_raw" {
-  value = data.external.env.result
+data "external" "env" {
+  program = ["jq", "-n", "env"]
 }
 
 output "data_home_directory" {
-  value = data.external.env.result.HOME
+  value = lookup(data.external.env.result, "HOME")
 }
+
+resource "null_resource" "testing_for_schematics" {
+  count = "${lookup(data.external.env.result, "HOME") == "/home/nobody"}" ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "echo \"This will run if in Schematics\""
+  }
+}
+
+resource "null_resource" "testing_for_home" {
+  count = "${lookup(data.external.env.result, "HOME") == "/Users/ryan"}" ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "echo \"This will run if in Home environment\""
+  }
+}
+
+
+# resource "local_file" "output" {
+#   count = "${lookup(data.external.env.result, "HOME") == "/Users/ryan" }" ? 1 : 0
+#   content = <<EOF
+# ${lookup(data.external.env.result, "HOME")}
+# EOF
+
+#   filename = "deployment.env"
+# }
